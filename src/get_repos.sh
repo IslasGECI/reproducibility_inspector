dc60b8e359eeeed804fbabec70b90760542a3160
Guillermo Olvera • GECI <guillermo.olvera@islas.org.mx>
2026-06-10 14:31:19 -0600

    👽🪣 Update bitbucket API access token request

--- src/get_repos.sh
+++ src/get_repos.sh
@@ -8,5 +8,5 @@ set -euo pipefail
 
 curl --request GET \
   --url 'https://api.bitbucket.org/2.0/repositories/IslasGECI?q=updated_on%20%3E%202019-07-11&sort=-updated_on&pagelen=100' \
-  --user ${BITBUCKET_USERNAME}:${BITBUCKET_PASSWORD} \
+  --user ${BITBUCKET_EMAIL}:${BITBUCKET_API_TOKEN} \
   --output /workdir/data/repos.json
